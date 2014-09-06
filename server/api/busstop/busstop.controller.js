f368b708a77095c8cfe6c66bd53a9f6c8671d007
Eric Lundmark <erilu506@gmail.com>
2014-09-06 14:39:37 +0200

    refactored nearestBusstop

--- server/api/busstop/busstop.controller.js
+++ server/api/busstop/busstop.controller.js
@@ -1,14 +1,11 @@
 'use strict';
 
-var currentLocation;
-var http = require("http");
-var https = require("https");
 var _ = require('lodash');
 var Busstop = require('./busstop.model');
+var Utils = require('../../components/util/util');
 
 // Get list of busstops
 exports.index = function(req, res) {
-	console.log("INDEX");
 	Busstop.find(function (err, busstops) {
 		if(err) { return handleError(res, err); }
 		return res.json(200, busstops);
@@ -17,7 +14,6 @@ exports.index = function(req, res) {
 
 // Get a single busstop
 exports.show = function(req, res) {
-	console.log("asdfasd");
 	Busstop.findById(req.params.id, function (err, busstop) {
 		if(err) { return handleError(res, err); }
 		if(!busstop) { return res.send(404); }
@@ -62,40 +58,19 @@ exports.destroy = function(req, res) {
 };
 
 exports.currentLocation = function(req, res) {
-
 	var position = JSON.parse(req.params.currentLocation);
-	var closestBusstop;
-	var radius = 500;
-	var str = '';
-	var options = {
-		method: 'GET',
-		host: 'api.trafiklab.se',
-		path: '/samtrafiken/resrobot/StationsInZone.json'+
-		'?key=DgKtW2dvK9XZRnjrYeXhptwDJP6RDUNj'+
-		'&centerX='+position.xCoord+
-		'&centerY='+position.yCoord+
-		'&radius=' + radius+
-		'&coordSys=WGS84'+
-		'&apiVersion=2.1'
-	};
-	var req = https.request(options, function(response){
-		response.on('data', function (chunk) {
-			str += chunk;
+	Utils.nearestBusstop(position, function(busstop){
+		var nearestBusstop = {
+			'id':busstop["@id"],
+			"name": busstop.name,
+			"location": [busstop["@x"], busstop["@y"]]
+		};
+		Busstop.findById(7410957, function(err, busstop) {
+			if(err) { return handleError(res ,err); }
+			if(!busstop) {return res.send(404); }
+			return res.json(200,busstop); 
 		});
-		response.on('end', function () {
-			currentLocation = JSON.parse(str).stationsinzoneresult.location[0];
-			closestBusstop = {
-				'id':currentLocation["@id"],
-				"name": currentLocation.name,
-				"location": [currentLocation["@x"], currentLocation["@y"]]
-			};
-			Busstop.findById(7410957, function(err, busstop) {
-				if(err) { return handleError(res ,err); }
-				if(!busstop) {return res.send(404); }
-				return res.json(200,busstop.events); 
-			});
-		});
-	}).end();
+	}, handleError);
 };
 
 function handleError(res, err) {
---
+++ server/components/util/util.js
@@ -0,0 +1,31 @@
+'use strict';
+var https = require("https");
+
+module.exports.nearestBusstop = function(position, success, error){
+	var radius = 500;
+	var str = '';
+	var options = {
+		method: 'GET',
+		host: 'api.trafiklab.se',
+		path: '/samtrafiken/resrobot/StationsInZone.json'+
+		'?key=DgKtW2dvK9XZRnjrYeXhptwDJP6RDUNj'+
+		'&centerX='+position.xCoord+
+		'&centerY='+position.yCoord+
+		'&radius=' + radius+
+		'&coordSys=WGS84'+
+		'&apiVersion=2.1'
+	};
+	var req = https.request(options, function(response){
+		response.on('data', function (chunk) {
+			str += chunk;
+		});
+		response.on('end', function () {
+			var result = JSON.parse(str).stationsinzoneresult.location[0];
+			if (result) {
+				success(result);
+			}else{
+				error(404);
+			}
+		});
+	}).end();
+}
