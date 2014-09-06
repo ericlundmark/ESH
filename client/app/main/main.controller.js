79e01999edd374d9d89eb2aa8fd5ec621b2d8498
nicsc160 <nicsc160@student.liu.se>
2014-09-06 13:20:54 +0200

    hämta närmast buss hållplats

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var pos = {
+		'yCoord':'58.394127',
+		'xCoord':'15.561469'
+};
 angular.module('eshApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
+    $http.get('/api/busstops/123/' + JSON.stringify(pos)).success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
+      console.log($scope.awesomeThings);
     });
 
     $scope.addThing = function() {
