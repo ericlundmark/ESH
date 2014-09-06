c4a6fd86d1e5aff77f4e61c61b391a76b5e7a822
Fredrik Rosenqvist <frero718@student.liu.se>
2014-09-06 16:28:09 +0200

    add function for GPS-positioning

--- client/app/event/event.controller.js
+++ client/app/event/event.controller.js
@@ -9,9 +9,21 @@ angular.module('eshApp')
   .controller('EventCtrl', function ($scope, $http) {
 
     $scope.busstop = {};
-
+    $scope.cord;
     $http.get('/api/busstops/').success(function(busstops) {
     	$scope.busstop = busstops[0];
     	console.log($scope.busstop)
     });
+
+     $scope.getCurrentLocation=function(){
+      var pos;
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          pos={'lon':position.coords.longitude,'lat':position.coords.latitude};
+        });
+      } else {
+        console.log("No location could be found");
+      }
+      return pos;
+    }
 });
