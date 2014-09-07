517dbbab0eba66acbc58ab801d7d98559fea857b
Eric Lundmark <erilu506@gmail.com>
2014-09-07 11:46:51 +0200

    fixed favorite

--- client/app/event/show/show.controller.js
+++ client/app/event/show/show.controller.js
@@ -33,28 +33,50 @@ angular.module('eshApp')
     	});
     	map.panTo(center);
     });
-    $scope.likeEvent = function(e){
-    	$http.put('/api/users/'+Auth.getCurrentUser()._id + "/" + e._id ).success(function(event) {
+    $scope.isFavorite = function(eventId){
+    	var events = Auth.getCurrentUser().events;
+    	if (events != undefined && events.length==0) {return false};
+    	var event = _.findWhere(events, {_id: eventId});
+    	return event != undefined;
+    }
+    $scope.toggleFavorite = function(event){
+    	var user = Auth.getCurrentUser();
+    	if (user != undefined && user.events!= undefined && user.events.length==0) {
+    		user.events.push({
+    			_id: event._id,
+    			name: event.name
+    		});
+    	}else{
+    		var result = _.findWhere(user.events, {_id: event._id});
+    		if (result) {
+    			var index = user.events.indexOf({
+    				_id: event._id,
+    				name: event.name
+    			});
+    			user.events.splice(index, 1);
+    		}
+    	}
+    	$http.put('/api/users/'+user._id + "/" + event._id ).success(function(event) {
     		console.log(event);
-    	});; 
-    };
+    	});
+    }
 
     $scope.isSun = function(regn) {
 
-			return regn >=3;
-		}
+    	return regn >=3;
+    }
 
     var weekDays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
 
     $scope.getDay = function(index){
-        var i = index;
-        if (i == 0) {
-            return "Idag"
-        }else if (i == 1) {
-            return "Imorgon "
-        }else {
-            return weekDays[i];
-        };
-        
+    	var i = index;
+    	if (i == 0) {
+    		return "Idag"
+    	}else if (i == 1) {
+    		return "Imorgon "
+    	}else {
+    		return weekDays[i];
+    	};
+    	
     }
-  });
+});
