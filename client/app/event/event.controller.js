0c47bce2cd66702d2e38bf92944386a0d915773d
Eric Lundmark <erilu506@gmail.com>
2014-09-07 10:37:18 +0200

    stuff

--- client/app/event/event.controller.js
+++ client/app/event/event.controller.js
@@ -15,6 +15,28 @@ angular.module('eshApp')
 		var event = _.findWhere(events, {_id: eventId});
 		return event != undefined;
 	}
+	$scope.toggleFavorite = function(event){
+		var user = Auth.getCurrentUser();
+		if (user.events.length==0) {
+			user.events.push({
+				_id: event._id,
+				name: event.name
+			});
+		}else{
+			var result = _.findWhere(user.events, {_id: event._id});
+			if (result) {
+				var index = user.events.indexOf({
+					_id: event._id,
+					name: event.name
+				});
+				user.events.splice(index, 1);
+			}
+		}
+		$http.put('/api/users/' + user._id, user).success(function(user){
+			console.log('success');
+			console.log(user);
+		});
+	}
 	getCurrentLocation(function(loca) {
 		$http.get('/api/busstops/-1/'+JSON.stringify(loca))
 		.success(function(busstop) {
